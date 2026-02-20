fed71b64bfa1cfdcb2f22e683a426c06820afc96
Biniyam <biniyamdaniel.47@gmail.com>
2026-02-20 07:58:05 +0300

    fix: mac os compatability issue

--- source.config.ts
+++ source.config.ts
@@ -8,7 +8,6 @@ import {
 import { transformerTwoslash } from 'fumadocs-twoslash';
 import { createFileSystemTypesCache } from 'fumadocs-twoslash/cache-fs';
 import remarkMath from 'remark-math';
-import { remarkTypeScriptToJavaScript } from 'fumadocs-docgen/remark-ts2js';
 import rehypeKatex from 'rehype-katex';
 import { z } from 'zod';
 import {
@@ -99,7 +98,6 @@ export default defineConfig({
       remarkSteps,
       remarkMath,
       remarkAutoTypeTable,
-      remarkTypeScriptToJavaScript,
     ],
     rehypePlugins: (v) => [rehypeKatex, ...v],
   },
